aea6ffa7bfe3756a75ff8314ff29abe1b64395b1
Rahul <rahulforallp@gmail.com>
2016-06-14 03:02:12 +0530

    all done

---
+++ src/main/scala/execute/main.scala
@@ -0,0 +1,10 @@
+package execute
+import example.Lists
+
+/**
+  * Created by knoldus on 14/6/16.
+  */
+object main extends App {
+  println("Max : "+Lists.max(List(1,3,2)))
+  println("Sum : "+Lists.sum(List(1,3,2)))
+}
